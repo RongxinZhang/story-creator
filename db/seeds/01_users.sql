d9ddbdc4fd6ee9027dc665fc4025697b4a5567e8
Jeffrey Shao <shaoyuhao970909@gmail.com>
2020-08-22 04:56:15 +0000

    four tables are created, four instances are created for each table

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,6 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (username, first_name, last_name, email, password, created_at) 
+VALUES ('skater_boy', 'Bob', 'Smith', 'bskater@mail.com', 'password', '2019-02-10'),
+ ('awesome_guy', 'Andrew','chow','andrew12345@gmail.com','password', '2020-08-21'),
+ ('messedup_guy', 'Jacob','Ba','jabb78@gmail.com', 'password', '2020-07-25'),
+ ('booknerd', 'Lisa','Chow','chow12345@gmail.com', 'password', '2010-12-01');
