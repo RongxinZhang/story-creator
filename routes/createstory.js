084da534e086d82a8921876b24523e4d4bee66a5
Jenny Gao <gao.jenny9@gmail.com>
2020-08-23 18:17:52 +0000

    initial ejs form  for create story page

---
+++ routes/createstory.js
@@ -0,0 +1,41 @@
+/*
+ * All routes for Story-Creator are defined here
+ * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
+ */
+
+const express = require('express');
+const router  = express.Router();
+
+
+// const createStory = (db) => {
+//   router.post("/", (req, res) => {
+//     let query = `INSERT INTO stories 
+//       (owner_id, title, content, photo_url)
+//       VALUES ($1, $2, $3, $4) RETURNING *;`;
+
+//     const inputValues = [
+//       req.body.owner_id,
+//       req.body.title,
+//       req.body.content,
+//       req.body.photo_url
+//     ];
+
+//     console.log(req.body);
+//     console.log(inputValues);
+
+//     db.query(query, inputValues)
+//       .then(data => {
+//         const stories = data.rows;
+//         console.log(stories);
+//         res.json({ stories });
+//       })
+//       .catch(err => {
+//         res
+//           .status(500)
+//           .json({ error: err.message });
+//       });
+//   });
+//   return router;
+// };
+
+module.exports = {createStory};
