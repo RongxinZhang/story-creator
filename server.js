798b59c240e38860a9988768fd10faaf4c1eb0e0
Jeffrey Shao <shaoyuhao970909@gmail.com>
2020-08-24 04:47:00 +0000

    something get routes

--- server.js
+++ server.js
@@ -53,9 +53,9 @@ app.use("/register", registerUser(db));
 // Home page
 // Warning: avoid creating more routes in this file!
 // Separate them into separate routes files (see above).
-app.get("/", (req, res) => {
-  res.render("index");
-});
+// app.get("/", (req, res) => {
+//   res.render("index");
+// });
 
 
 
