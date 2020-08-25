24933059e33142d7e96077971165611da1228bcc
Jenny Gao <gao.jenny9@gmail.com>
2020-08-25 02:57:45 +0000

    add more html to create contribution function

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -7,16 +7,27 @@ $(() => {
     return p.innerHTML;
   };
 
+
+  // sample input object "contribution":
+  // { username: "skater_boy", content: "He stood up and stared", created_at: 2020-07-28 00:00:00, }
+
   const createPostElement = function (contribution) {
     let $contribution = `
       <article>
         <header>
+          <div>
+            <h5>${contribution.username}</h5>
+          </div>
         </header>
         <div>
+          <p>${escape(contribution.content)}</p>
         </div>
         <footer>
+          <div>${contribution.created_at}</div>
+          <div>//insert like icon</div>
         </footer>
       </article>
     `;
   }
+  return $contribution;
 });
