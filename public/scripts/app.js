29c18ee21f7b2b2ec3b869dff85ed26b00fd8a41
Jenny Gao <gao.jenny9@gmail.com>
2020-08-25 02:48:52 +0000

    add initial create post script

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,22 @@
 $(() => {
-  $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
-    }
-  });;
+
+  // stringify user input before parsing in to prevent XSS
+  const escape = function(str) {
+    let p = document.createElement('p');
+    p.appendChild(document.createTextNode(str));
+    return p.innerHTML;
+  };
+
+  const createPostElement = function (contribution) {
+    let $contribution = `
+      <article>
+        <header>
+        </header>
+        <div>
+        </div>
+        <footer>
+        </footer>
+      </article>
+    `;
+  }
 });
