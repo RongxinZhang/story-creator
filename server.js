1d81ab7a021120750301073aded44fa27c33e1db
Rongxin Zhang <mrrongxin@gmail.com>
2020-08-26 11:40:42 -0700

    remove extra public file middleware

--- server.js
+++ server.js
@@ -17,7 +17,6 @@ const dbParams = require('./lib/db.js');
 const db = new Pool(dbParams);
 db.connect();
 
-app.use(express.static("public"));
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
@@ -39,6 +38,7 @@ app.use("/styles", sass({
   debug: true,
   outputStyle: 'expanded'
 }));
+
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({
   extended: true
