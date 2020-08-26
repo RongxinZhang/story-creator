2d964ccaa3a4109295515c6348041ac2f2bcdd51
Jeffrey Shao <shaoyuhao970909@gmail.com>
2020-08-26 21:49:04 +0000

    implement the jquery for detecting if user got empty field

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -9,9 +9,26 @@
 //   });;
 // });
 
+
 $(document).ready(function(){
-  $()
+  // console.log($('section form .input'));
+ $('.login-form').submit(function(event){
+  $('section form .input').each(function(index, input){
+    const data = $(input);
+    if(!data.val()){
+      // console.log('there no value!!!',data.attr('name'));
+      event.preventDefault();
+      $('section .alert').slideDown();
+    }else{
+      $('login-form').submit();
+    }
+  })
+ })
 })
+
+  
+
+
 
 
 
