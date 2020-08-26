c0ec3cfefc2238a8e4c116239b33c5ff1010cf56
Rongxin Zhang <mrrongxin@gmail.com>
2020-08-26 16:13:53 -0700

    Authentication route

--- routes/submitRegister.js
+++ routes/submitRegister.js
@@ -8,40 +8,44 @@
 const express = require('express');
 const router = express.Router();
 
+const bcrypt = require('bcrypt');
+const SALT_ROUNDS = 10;
 
 const submitRegister = (db) => {
   router.post('/', (req, res) => {
-    const queryString1 =`SELECT username FROM users WHERE username =$1;`
+    const queryString1 = `SELECT username FROM users WHERE username =$1;`;
+    const queryString2 = `INSERT INTO users ( username, first_name, last_name, email, password) VALUES($1, $2, $3, $4, $5)
+        RETURNING *;`;
     const inputField1 = [req.body.username];
-     db.query(queryString1, inputField1)
-     .then(data => {
-      const users = data.rows;
-      if(data.rows[0]){
-        res.send("error: duplicate username");
-      }else{
-        const queryString = `INSERT INTO users ( username, first_name, last_name, email, password) VALUES($1, $2, $3, $4, $5)
-        RETURNING *;`
+    
+    db.query(queryString1, inputField1)
+      .then(data => {
+        if (data.rows[0]) {
+          throw (Error("error: duplicate username"));
+        }
+
+        const passwordHash = bcrypt.hashSync(req.body.email, SALT_ROUNDS);
+
         const inputValue = [
           req.body.username,
           req.body.first_name,
           req.body.last_name,
           req.body.email,
-          req.body.password,
+          passwordHash
         ];
-        db.query(queryString, inputValue)
-        .then(data => {
-          const users = data.rows;
-          res.json({ users });
-        })
-        .catch(err => {
-          res
-            .status(500)
-            .json({ error: err.message })
-        })
-      }
-    })
-  
-  })
+
+        return db.query(queryString2, inputValue);
+      })
+      .then(data => {
+        // res.json({userCreated: true });
+        res.json({data});
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
   return router;
-}
-module.exports = { submitRegister } ;
+};
+module.exports = { submitRegister };
