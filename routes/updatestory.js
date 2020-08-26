7d60d20f2a3f6162e7a2df7303313e5d30e1c80f
Jenny Gao <gao.jenny9@gmail.com>
2020-08-26 23:08:46 +0000

    res.redirect upon update story

--- routes/updatestory.js
+++ routes/updatestory.js
@@ -42,8 +42,9 @@ module.exports = (db) => {
     return db.query(queryString, queryParams)
       .then(result => {
         const story = result.rows[0];
-        res.send({story: result.rows[0], message: "successfully updated"})
-        // res.render('ejspage', story)
+        // res.send({story: result.rows[0], message: "successfully updated"})
+        // res.render('story', story)
+        res.redirect(`/story/${story.storyurl_id}`);
       })
       .catch(err => {
         console.error(err);
