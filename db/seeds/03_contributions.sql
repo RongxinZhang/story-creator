993721ca29765768cd016bba05dbf97690d9c15a
Rongxin Zhang <mrrongxin@gmail.com>
2020-08-28 08:11:44 -0700

    Add styling for completed stories

--- db/seeds/03_contributions.sql
+++ db/seeds/03_contributions.sql
@@ -6,4 +6,5 @@ VALUES
 (1, 3, 'He stood up and stared across the river and pondered if he should cross.', FALSE, '2020-07-05'),
 (1, 4, 'contribution A: user 1, story 3.', FALSE, '2020-06-23'),
 (2, 4, 'contribution D: user 3, story 4', FALSE, '2020-07-28'),
-(3, 4, 'contribution B: user 3, story 4', TRUE, '2020-07-28');
+(3, 4, 'Lyncon entered the village with his sword drawn, baring the blue-grey blade. The village was deathly quiet, not a creature stirred while even the wind seemed absent, instead the air feeling heavy with foreboding. Lyncon moved cautiously forward, treading lightly while his eye flitted from one spot to the next. Beside him, Vanhelm sensed it too and sniffed the air, his tail stilled.', TRUE, '2020-07-28'),
+(3, 4, 'Lyncon entered the village with his sword drawn, baring the blue-grey blade. The village was deathly quiet, not a creature stirred while even the wind seemed absent, instead the air feeling heavy with foreboding. Lyncon moved cautiously forward, treading lightly while his eye flitted from one spot to the next. Beside him, Vanhelm sensed it too and sniffed the air, his tail stilled.', TRUE, '2020-07-28');
